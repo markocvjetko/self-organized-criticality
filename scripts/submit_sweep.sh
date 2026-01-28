c702dcba5f36bd9173163793d1f106b8120507ba
markocvjetko <marko.cvjetko1@gmail.com>
2026-01-28 23:06:32 +0100

    fix sweep script

--- scripts/submit_sweep.sh
+++ scripts/submit_sweep.sh
@@ -3,7 +3,8 @@
 
 for board_size in 100 150 200 300 400 500 1000; do
     for num_perturbations in 100000 1000000; do
-    sbatch --export=ALL,BOARD_SIZE=$board_size \
+    sbatch --export=ALL,BOARD_SIZE=$board_size,NUM_PERTURBATIONS=$num_perturbations \
         ./launch_experiments.sh
-    echo "Submitted: board=$board_size"
+    echo "Submitted: board=$board_size, num_perturbations=$num_perturbations"
+    done
 done
